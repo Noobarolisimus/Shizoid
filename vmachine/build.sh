58b2cf42fb57ae58510271709173bd4f35d8723a
Noobarolisimus <kek.vavilov@ya.ru>
2024-02-08 18:52:07 +0400

    build.sh fix

--- vmachine/build.sh
+++ vmachine/build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-premake5 export-compile-commands --cc=clang gmake2 
-echo "Copying compile_commands.json ..."
-if [ -z $1 ]
+
+mode=debug
+
+if [ $# -gt 1 ]
 then
-    cp compile_commands/debug_linux.json compile_commands.json
-    make config=debug_linux
-else
-    cp compile_commands/debug_linux.json compile_commands.json
-    make config=$1_linux
+    mode="$1"
 fi
+
+if [ -f "~/.premake/export-compile-commands.lua" ]
+then
+    premake5 export-compile-commands --cc=clang gmake2 
+    echo "Copying compile_commands.json ..."
+    cp compile_commands/debug_linux.json compile_commands.json
+else
+    premake5 --cc=clang gmake2
+fi
+
+make "config=${mode}_linux"
